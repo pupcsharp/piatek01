e924ee0a71b135d0a10a863867ab81dba4775af6
JolaLojalna <pupcsharp@gmail.com>
2014-06-20 12:16:49 +0200

    cos nowego

---
+++ jedyneczka/jedyneczka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jedyneczka
+{
+    class Pierwsza
+    {
+        int Id { get; set; }
+        string nazwa { get; set; }
+        public Pierwsza(int id, string nazwa)
+        {
+            this.Id = id;
+            this.nazwa = nazwa;
+
+        }      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
